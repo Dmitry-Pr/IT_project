6380cadccfd71655a8b026ea6c7aa8572efd28e3
Dmitry-Pr <61019019+Dmitry-Pr@users.noreply.github.com>
2021-11-08 21:50:20 +0300

    Update loading.py

--- PROJECT/loading.py
+++ PROJECT/loading.py
@@ -3,5 +3,5 @@ from kivymd.uix.screen import MDScreen
 
 class LoadingScreen(MDScreen):
     """Class of loading screen that is shown
-     while long loadings in some trainings."""
+     during long loadings in some trainings."""
     pass
